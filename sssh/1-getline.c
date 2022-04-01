94e79f1e3d42f9e8eb7ce490a28f2c3596fbd0ea
Agustin Labadie <4433@holbertonschool.com>
2022-04-01 11:02:48 -0700

    Getline loops till -1

--- sssh/1-getline.c
+++ sssh/1-getline.c
@@ -6,10 +6,14 @@ int main (void)
 	size_t num = 1024;
 	char *buf = NULL;
 	FILE *str;
+	int gl_r;
 
-	str = fopen("asdf", "r");
-
-	getline(&buf, &num, str);
-	printf("%s\n", buf);
+	str = stdin;
+	for (;gl_r != -1;)
+	{
+	printf("$ ");
+	gl_r = getline(&buf, &num, str);
+	printf("%s", buf);
+	}
 	return (1);
 }
