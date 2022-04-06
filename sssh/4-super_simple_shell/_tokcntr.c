be27e57321ca937abe11af6f23618cd519e0c2eb
Agustin Labadie <4433@holbertonschool.com>
2022-04-06 12:50:07 -0700

    BEtty and border case

--- sssh/4-super_simple_shell/_tokcntr.c
+++ sssh/4-super_simple_shell/_tokcntr.c
@@ -2,13 +2,16 @@
 /**
  *_tokcntr - Counts tokens in a string parsed by *token.
  *@str: Line to be tokenized.
- *@token: Separator.
+ *@separator: Separator.
  *Return: Amount of counted separator plus one.
  */
 int _tokcntr(char *str, char *separator)
 {
 	int i;
 	int a = 1;
+
+	if (separator[1] != '\0')
+		return (-1);
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
