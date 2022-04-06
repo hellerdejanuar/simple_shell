1beca8fee7e4b97ac55e4fec5e535052092699ca
Agustin Labadie <4433@holbertonschool.com>
2022-04-06 10:28:08 -0700

    SSS without external func

--- sssh/4-super_simple_shell/4-super_simple_shell.c
+++ sssh/4-super_simple_shell/4-super_simple_shell.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#define _PATH "/bin/"
 /**
  * main - runs getline_tok to read a line and tokenize the string into a list
  * Return: 0 success
@@ -7,22 +7,39 @@
 int main(void)
 {
 	char **index = NULL;
-	char *argv[] ={"ls", "-l", "./", NULL}; 
-	char *env[] = {"/bin/ls", NULL}; /* PATH ? */
+	char *argv[] = NULL;
+	char *env[] = NULL;
+	char *pathname;
+	unsigned int arg_cn = 0;
 
 	while (1)
 	{
-		/* getline function. returns index to */
+		/* getline function. returns index of arguments*/
 		index = _getline_tok();
-
+		/*if _getline_tok return NULL if fails or EOF*/
 		if (index == NULL)
 			break;
+		pathname = malloc(sizeof(char) * (_strlen(index[0]) + _strlen("/bin/")));
+		pathname = _strconcat(_PATH, index[0]);
 
-		execve("/bin/ls", argv, env);
+		arg_cn = _list_cntr(index);
+		/* Allocates argv */
+
+		argv = malloc(arg_cn * sizeof(char *));
+
+		/*Loop through index[] to set argv[]*/
+		for (i = 0; index[i] != NULL; i++)
+		{
+		argv[i] = index[i];
+		}
+		argv[i] = index[i];
+
+		execve(pathname, argv, env);
 		/* for (i = 0; index[i] != NULL; i++)
 			printf("%s\n", index[i]); */
 
 		free(index);
+		free(pathname);
 	}
 
 	return (0);
