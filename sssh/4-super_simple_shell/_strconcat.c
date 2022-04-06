1d1a4292e04d941c34f75e215bc74a4d0f0cc264
Agustin Labadie <4433@holbertonschool.com>
2022-04-06 12:40:50 -0700

    Added betty and border cases int _strconcat.c

--- sssh/4-super_simple_shell/_strconcat.c
+++ sssh/4-super_simple_shell/_strconcat.c
@@ -3,7 +3,7 @@
  *_strconcat - Concatenates two strings, adds '\0' at end.
  *@s1: First string.
  *@s2: Second string.
- *Return: s3, being a string ending in '\0'.
+ *Return: s3, being a string ending in '\0'. NULL if error.
  */
 
 char *_strconcat(char *s1, char *s2)
@@ -13,18 +13,25 @@ char *_strconcat(char *s1, char *s2)
 	unsigned int b = 0;
 	unsigned int i;
 
+	if (!s1 || !s2)
+		return (NULL);
+
+	/*Sets a to character quantity of s1*/
 	for (i = 0 ; s1[i] != '\0'; i++)
 	{
 		a++;
 	}
 
+	/*Sets b to character quantity of s2*/
 	for (i = 0; s2[i] != '\0'; i++)
 	{
 		b++;
 	}
 
+	/*Allocates final string before merging*/
 	s3 = malloc(sizeof(char) * (a + b + 1));
 
+	/*Iterates trough s3 to set every value of the final string*/
 	for (i = 0; i < a; i++)
 	{
 		s3[i] = s1[i];
@@ -33,6 +40,7 @@ char *_strconcat(char *s1, char *s2)
 	{
 		s3[i] = s2[i - a];
 	}
+	/*Adds last null char to string to make it valid*/
 	s3[i] = '\0';
 
 	return (s3);
