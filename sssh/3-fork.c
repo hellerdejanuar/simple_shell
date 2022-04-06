48b7af9f62c3fe80a5c22ea77b27c2040253c853
Agustin Labadie <4433@holbertonschool.com>
2022-04-05 17:45:52 -0700

    EXERCISE FORKEXECVEFORK

--- sssh/3-fork.c
+++ sssh/3-fork.c
@@ -12,27 +12,18 @@ int main(void)
 
 	for (i = 0; i < PROCESS_QTY; i++)
 	{
-
-		printf("BEFORE fork()getpid %i\n", getpid());
-		printf("BEFORE fork()getppid %i\n", getppid());
 		pids[i] = fork();
-		printf("AFTER fork()getpid %i\n", getpid());
-		printf("AFTER fork()getppid %i\n", getppid());
+
 		if (pids[i] == -1)
     		printf("Error with creating process\n");
 
-		if (pids[i] != 0)
+		if (pids[i] == 0)
 		{
-			printf("IM A CHILD!! pid: %i\n", pids[i]);
+			printf("IM A CHILD!! pid: %i\n", getpid());
 			execve("/bin/ls", argv, env);
 			return (0);
 		}
-	}
-	 
-	for (i = 0; i < PROCESS_QTY; i++)
-	{
 		wait(NULL);
 	}
-
 	return (0);
 }
