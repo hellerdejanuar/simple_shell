13bee467db79272a047c4cb1cbcdd0463e9d6612
hellerdejanuar <hellerdejanuar@gmail.com>
2022-04-06 09:14:51 -0700

    4 in place

---
+++ sssh/4-super_simple_shell/4-super_simple_shell.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * main - runs getline_tok to read a line and tokenize the string into a list
+ * Return: 0 success
+ */
+int main(void)
+{
+	char **index = NULL;
+	int i;
+
+	while (1)
+	{
+		/* getline function. returns index to */
+		index = _getline_tok();
+
+		if (index == NULL)
+			break;
+
+		/* for (i = 0; index[i] != NULL; i++)
+			printf("%s\n", index[i]); */
+
+		free(index);
+	}
+
+	return (0);
+}
