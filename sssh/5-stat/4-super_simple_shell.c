0c301d69bca9ac10a1a54c92c419136b52ee5139
Agustin Labadie <4433@holbertonschool.com>
2022-04-07 08:58:52 -0700

    Working pero muuyyy desprolijpo el 5-stat, verificar la muerte de los hijos que anda mal, chau me voy a comer

---
+++ sssh/5-stat/4-super_simple_shell.c
@@ -0,0 +1,57 @@
+#include "main.h"
+#define _PATH "/bin/"
+/**
+ * main - runs getline_tok to read a line and tokenize the string into a list
+ * Return: 0 success
+ */
+int main(void)
+{
+	char **index = NULL;
+	char *argv[] = {NULL};
+	char *env[] = {NULL};
+	char *pathname;
+	unsigned int arg_cn = 0;
+	int i = 0;
+	struct stat st;
+
+int processid;
+	while (1)
+	{
+		/* getline function. returns index of arguments*/
+		index = _getline_tok();
+		/*if _getline_tok return NULL if fails or EOF*/
+		if (index == NULL)
+			break;
+
+		pathname = malloc(sizeof(char) * ( _strlen(index[0] + _strlen(_PATH))));
+		pathname = _strconcat(_PATH, index[0]);
+
+		arg_cn = _list_cntr(index);
+		/* Allocates argv */
+
+		*argv = malloc(arg_cn * sizeof(char *));
+
+		/*Loop through index[] to set argv[]*/
+		for (i = 0; index[i] != NULL; i++)
+		{
+		argv[i] = index[i];
+		}
+		argv[i] = index[i];
+		i = 0;
+		/*If command not found, prints NOT FOUND*/
+		/*concatenate string to serch in _PATHargv[i]*/
+		if (stat(_strconcat(_PATH,argv[0]), &st) == -1)
+			printf("%s: NOT FOUND\n", argv[i]);
+		{
+			processid = fork();
+			if( processid == 0)
+			execve(pathname, argv, env);
+			/* for (i = 0; index[i] != NULL; i++)
+				printf("%s\n", index[i]); */
+			wait(NULL);
+		}
+		free(index);
+		free(pathname);
+	}
+	return (0);
+}
---
+++ sssh/5-stat/_token_to_av.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * returns a list of all the arguments tokenized 
+ * (with the last newline removed)
+ */
+char **_token_to_av(char *str, char* separator)
+{
+	char **index = NULL;
+	char *token = NULL;
+	int i, tok_cn, n;
+
+	/* counts the amount of tokens to be indexed*/
+	tok_cn = _tokcntr(str, separator);
+
+	/* allocates some space for the index */
+	index = malloc((tok_cn + 1) * sizeof(char *));
+
+	/* init strtok */
+	token = strtok(str, separator);
+
+	for (i = 0; token != NULL; i++)
+	{
+		index[i] = token;
+		token = strtok(NULL, separator);
+	}
+
+	index[i] = NULL;
+
+	for (n = 0; index[i - 1][n] != '\n'; n++)
+	{}
+
+	index[i - 1][n] = '\0';
+
+	return (index);
+}
