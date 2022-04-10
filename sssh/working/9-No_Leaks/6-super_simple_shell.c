d7392b5f013ea58dd3d5eb8f957635ecfbb78410
hellerdejanuar <hellerdejanuar@gmail.com>
2022-04-10 09:56:52 -0700

    No Leaks all working

---
+++ sssh/working/9-No_Leaks/6-super_simple_shell.c
@@ -0,0 +1,66 @@
+#include "main.h"
+#define _PATH "/bin/"
+/**
+ * main - runs getline_tok to read a line and tokenize the string into a list
+ * Return: 0 success
+ */
+int main(void)
+{
+	char **index = NULL;
+	char **env = NULL;
+	char *pathname = NULL;
+	int j = 0;
+	struct stat st;
+	int processid;
+	char **path = NULL;
+	int er_flag = 0;
+
+	path = _token_to_av(_getenv("PATH"), ":");
+
+	if (path == NULL)
+		printf("PATH not found\n");
+
+	while (1)
+	{
+		/* getline function. returns index of arguments*/
+		index = _getline_tok();
+		/*if _getline_tok return NULL if fails or EOF*/
+		if (index == NULL)
+			break;
+		
+		er_flag = 0;
+
+		for (j = 0; path[j] != NULL; j++)
+		{
+			pathname = malloc(sizeof(char) * (_strlen(index[0]) + _strlen(path[j]) + 1));
+			pathname = _strconcat(path[j], "/");
+			pathname = _strconcat(pathname, index[0]);
+			
+			/*If command not found, pr*/
+			if (stat(pathname, &st) == -1)
+			{
+				er_flag = 0;
+			}
+			else
+			{
+				processid = fork();
+				/*Children process strats*/
+				if( processid == 0)
+				{
+					execve(pathname, index, env);
+					/*Kill children when execve fails*/
+					exit(0);
+				}
+				wait(NULL);
+				er_flag = 1;
+				break;
+			}
+		}
+		/*Outside for-loop, if command not found on any PATH*/
+		if (er_flag == 0)	
+			printf("%s: Command not found.\n", index[0]);
+
+		free(pathname);
+	}
+	return (0);
+}
---
+++ sssh/working/9-No_Leaks/_strconcat.c
@@ -0,0 +1,47 @@
+#include "main.h"
+/**
+ *_strconcat - Concatenates two strings, adds '\0' at end.
+ *@s1: First string.
+ *@s2: Second string.
+ *Return: s3, being a string ending in '\0'. NULL if error.
+ */
+
+char *_strconcat(char *s1, char *s2)
+{
+	char *s3;
+	unsigned int a = 0;
+	unsigned int b = 0;
+	unsigned int i;
+
+	if (!s1 || !s2)
+		return (NULL);
+
+	/*Sets a to character quantity of s1*/
+	for (i = 0 ; s1[i] != '\0'; i++)
+	{
+		a++;
+	}
+
+	/*Sets b to character quantity of s2*/
+	for (i = 0; s2[i] != '\0'; i++)
+	{
+		b++;
+	}
+
+	/*Allocates final string before merging*/
+	s3 = malloc(sizeof(char) * (a + b + 1));
+
+	/*Iterates trough s3 to set every value of the final string*/
+	for (i = 0; i < a; i++)
+	{
+		s3[i] = s1[i];
+	}
+	for (; i < (a + b); i++)
+	{
+		s3[i] = s2[i - a];
+	}
+	/*Adds last null char to string to make it valid*/
+	s3[i] = '\0';
+
+	return (s3);
+}
