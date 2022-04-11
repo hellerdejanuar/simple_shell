8ee38d7d2d7b01dad2910369239e7eb5aa9fb037
Agustin Labadie <4433@holbertonschool.com>
2022-04-11 15:33:17 -0700

    checking \n case

--- sssh/9-No_Leaks/_getline_tok.c
+++ sssh/9-No_Leaks/_getline_tok.c
@@ -21,7 +21,11 @@ char **_getline_tok(void)
 		/* This exceptions could be handled better with the return EOF of getline */
 		if (gl_r == -1 || _strcmp("end-of-file\n", buf) || _strcmp("EOF\n", buf))
 			break;
-
+		if (buf[0] == '\n')
+		{
+			index[0] = "\n";
+			return (index);
+		}
 		index = _token_to_av(buf, " ");
 
 		return (index);
