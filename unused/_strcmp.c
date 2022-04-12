66a1a4f1c20963833aaa2177dd8c3aa8242f8713
Agustin Labadie <4433@holbertonschool.com>
2022-04-11 19:13:21 -0700

    added env

---
+++ unused/_strcmp.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ * @s1: first string to compare
+ * @s2: second string to compare
+ *
+ * Return: 1 if s1 equal to s2, 0 if they're different,-1 if error.
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	if (!s1 || !s2)
+		return (-1);
+
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+			return (0);
+	}
+
+	if (s2[i] != '\0')
+		return (0);
+
+	return (1);
+}
